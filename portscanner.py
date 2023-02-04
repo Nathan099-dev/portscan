d3123695a57c56e0701abe91b6384be5ee46f21d
nathan099-dev <tonininathan8@gmail.com>
2023-02-03 21:35:40 -0300

    modificação no codigo

--- portscanner.py
+++ portscanner.py
@@ -1,36 +1,30 @@
 import socket
-import re 
+import sys
 
-ip_add_pattern = re.compile("^(?:[0-9]{1,3}\.){3} [0-9] {1,3}$")
-port_range_pattern = re.compile("([0-9]+) - ([0-9]+)")
-port_min = 0
-port_max = 65535
+def scann(target_ip, portas):
+	target_ip = str("Digite seu alvo")
+	portas = []
 
-open_ports = []
+	try:
+		for portas in range(1, 65535):
+			sys.argv[1]
+			sys.argv[2]
 
-while True:
-	print('Entre com o número de portas que você deseja escanear no formato:<int>.<int>')
-	port_range_valid = port_range_pattern.search(port_range.replace("",""))
+			s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+			s.connect_ex((target_ip, portas))
 
-	if port_range_valid:
-		port_min = int(port_range_valid.group[1])
-		port_max = int(port_range_valid.group[2])
-		break
+		if portas == 0:
+			print('porta, {} ====> ABERTA'.format(portas)) 
 
-		if port in range(port_min, port_max + 1):
-			try:
-				with socket.socket(socket.AF_INET, socket.SOCK_STREAM) as s;
-				s.settimeout(0.5)
-				s.connect((ip_add_entered, port))
-				open_ports.opened(portas)
-			except:
-				pass
+	except KeyboardInterrupt:
+		print('interrompido')
+		sys.exit()
 
-		for port in open_ports:
-			print(f'porta {portas} está aberta em {ip_add_entered}.')
+	except socket.gaierror:
+		print('Não resolvido')
+		sys.exit()
+
+	return scann()
 
 
 
-
-
-	
