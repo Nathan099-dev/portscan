7071b8f7fd6ada95e876cb78609f737c6d31f8b1
Nathan099-dev <tonininathan8@gmail.com>
2024-07-06 18:04:08 -0300

    melhorando código

--- portscanner.py
+++ portscanner.py
@@ -1,14 +1,23 @@
 import pyfiglet
 import socket
 import sys
+import os
+import colorama
+from colorama import Fore, init
 from datetime import datetime
 
 ascii_banner = pyfiglet.figlet_format('Python portscan')
 print(ascii_banner)
 
+os.system('cls')
+
+init()
+GREEN = Fore.GREEN
+GRAY = Fore.LIGHTBLACK_EX
+
 def scanner(target, i):
   target = str(input('Digite o seu alvo:' + ' '))
-
+  
   print("=" * 100)
   print(f'Escaneando alvo: {target}')
   print("escaneando alvo em" + ' ' + str(datetime.now()))
@@ -18,18 +27,19 @@ def scanner(target, i):
     for i in range(1, 65535):
       s  = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
       s.settimeout(.5)
-      res = s.connect_ex((target, i))
+      response = s.connect_ex((target, i))
 
-      if res == 0:
-        print(f'{i}, opened')
+      if response == 0:
+        print(f' {GREEN} [+] {i}' + ' ' + 'opened')
+        
       else:
           pass
 
   except KeyboardInterrupt:
-    print('Atalho CTRL + C pressionado... Interrompendo escaneamento')
+    print(f'{GRAY} Atalho CTRL + C pressionado... Interrompendo escaneamento')
     sys.exit()
 
-  except SystemError:
+  except SystemError: 
     sys.exit()
 
 scanner(target='', i='')
